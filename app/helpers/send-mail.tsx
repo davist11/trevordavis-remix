a0bf23853bd16efb1c7da6157263bd3745cdbc92
Trevor Davis <davist11@gmail.com>
2022-01-07 11:14:39 -0500

    [#6] modify sender function

--- app/helpers/send-mail.tsx
+++ app/helpers/send-mail.tsx
@@ -8,18 +8,21 @@ interface MessageType {
     html: string
 }
 
+const sendRequest = async (msg: MessageType) => {
+    try {
+        await sgMail.send(msg)
+    } catch (error) {
+        console.error(error)
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    }
+}
+
 export const sendMail = (msg: MessageType) => {
     // Otherwise await the send email request
     sgMail.setApiKey(process.env.SENDGRID_KEY)
-    ;(async () => {
-        try {
-            await sgMail.send(msg)
-        } catch (error) {
-            console.error(error)
 
-            if (error.response) {
-                console.error(error.response.body)
-            }
-        }
-    })()
+    sendRequest(msg)
 }
