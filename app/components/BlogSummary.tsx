716a80860a6ed60b3c9f8325c20b13a2754e373e
Trevor Davis <trevor.davis@dutchie.com>
2022-04-21 11:23:17 -0400

    [#13] more refactoring

--- app/components/BlogSummary.tsx
+++ app/components/BlogSummary.tsx
@@ -6,17 +6,12 @@ type BlogSummaryProps = {
 }
 
 const BlogSummary = ({ typeHandle, body }: BlogSummaryProps) => {
+    const summary =
+        typeHandle === 'externalArticle' ? body : useBlogSummary(body)
+
     return (
         <div className="text -long">
-            {typeHandle === 'externalArticle' ? (
-                <div dangerouslySetInnerHTML={{ __html: body }}></div>
-            ) : (
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: useBlogSummary(body),
-                    }}
-                ></div>
-            )}
+            <div dangerouslySetInnerHTML={{ __html: summary }}></div>
         </div>
     )
 }
