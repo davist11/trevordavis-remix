6e1d07a42ffb100dd72596fe3451ce0afe030d39
Trevor Davis <trevor.davis@dutchie.com>
2023-01-26 17:43:06 -0500

    add php to highlighting

--- app/components/blocks/CodeBlock.tsx
+++ app/components/blocks/CodeBlock.tsx
@@ -3,18 +3,19 @@ import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
 import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
 import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
-
+import php from 'react-syntax-highlighter/dist/cjs/languages/prism/php'
 import nightOwl from 'react-syntax-highlighter/dist/cjs/styles/prism/night-owl'
 
 SyntaxHighlighter.registerLanguage('css', css)
 SyntaxHighlighter.registerLanguage('jsx', jsx)
 SyntaxHighlighter.registerLanguage('javascript', javascript)
+SyntaxHighlighter.registerLanguage('php', php)
 
 const langLookup: Record<string, string> = {
     css: 'css',
     javascript: 'javascript',
     jsx: 'jsx',
-    text: 'text',
+    php: 'php',
 }
 
 export type CodeBlockType = {
